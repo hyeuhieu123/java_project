ce0efad776a24d8a061d0c9053fbd8342829454a
hieu <nguyentrunghieuthptxp@gmail.com>
2025-04-17 11:00:29 +0700

    add

--- src/ra/edu/validate/Validator.java
+++ src/ra/edu/validate/Validator.java
@@ -8,7 +8,7 @@ import java.util.regex.Pattern;
 public class Validator {
     private static final String PHONE_VIETTEL_PREFIXES = "086|096|097|098|039|038|037|036|035|034|033|032";
     private static final String PHONE_VINAPHONE_PREFIXES = "091|094|088|083|084|085|081|082";
-    private static final String PHONE_MOBIFONE_PREFIXES = "070|079|077|076|078|089|090|093";
+    private static final String PHONE_MOBIFONE_PREFIXES = "070|079|077|076|078|089|090|093|096";
 
     public static int validateInteger(String message, Scanner scanner) {
         System.out.println(message);
